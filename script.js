ff2316cb5b011f2b00e972fbd6b1dabf5b5d5bc6
Maridon Romney <mkromney@gmail.com>
2023-05-10 22:24:02 -0600

    got some help from the instructor, Vincent and the TAs, will add their advice and begin again tomorrow.

--- script.js
+++ script.js
@@ -14,21 +14,94 @@ const resetButton = document.querySelector('.reset-btn');
 
 const timer = document.querySelector('.timer');
 
-let currentQuestion = 0;
+const timerDisplay = document.getElementById('timer');
+const startButton = document.getElementById('start-btn');
+
+let timeLeft = 120; // change to desired time limit maybe move it up. 
 let score = 0;
-let timeLeft = 120;
+
+startButton.addEventListener('click', () => {
+
+  timer = setInterval(() => {	
+    if (timeLeft <= 0) {
+      clearInterval(timer);
+      alert('Time is up!');
+      return;
+	}
+		timerDisplay.textContent = timeLeft;
+    	timeLeft--;
+	},1000);
+});
+
+
+
+// // Example function to check answer and reduce time if wrong
+// function checkAnswer(answer) {
+//   if (answer !== 'correct') {
+//     clearInterval(timer);
+//     let timeLeft = parseInt(timerDisplay.textContent) - 5;
+//     if (timeLeft < 0) {
+//       timeLeft = 0;
+
+	
+//     }
+//     timerDisplay.textContent = timeLeft;
+//     timer = setInterval(() => {
+//       if (timeLeft <= 0) {
+//         clearInterval(timer);
+//         alert('Time is up!');
+//         return;
+//       }
+//       timerDisplay.textContent = timeLeft;
+//       timeLeft--;
+//     }, 1000);
+//   }
+// }
+
+function checkAnswer(answer) {
+	//if (the persons answer was correct) add one to their score
+	// else if the person answered wrong, subtract time from timeLeft
+	timeLeft = timeLeft - 5;
+	console.log(timeLeft);
+}
+
+
+let currentQuestion = 0;
 let timerInterval;
 let highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 
 // questions array with objects containing question, options, and answer
+
 const questions = [
 	{
-		question: 'What is the capital of France?',
-		options: ['Paris', 'Berlin', 'Madrid', 'Rome'],
+		question: 'What is the capital of Israel?',
+		options: ['Tel Aviv', 'Berlin', 'Madrid', 'Rome'],
 		answer: 0
 	},
 	{
 		question: 'What is the Capital of Germany?',
-    options: ['Paris', 'Berlin', 'Madrid', 'Rome'],
-    answer: 1
-  }]
+    	options: ['Ber;om', 'Paris', 'Madrid', 'Rome'],
+    	answer: 1
+ 	},
+	 {
+		question: 'What is the Capital of USA?',
+    	options: ['Wash. D.C.', 'Berlin', 'Madrid', 'Rome'],
+    	answer: 1
+ 	},
+	{
+		question: 'What is the Capital of Italy?',
+    	options: ['Rome', 'Berlin', 'Madrid', 'Rome'],
+    	answer: 1
+ 	},
+	{
+		question: 'What is the Capital of Sweden?',
+    	options: ['Helsinki', 'Berlin', 'Madrid', 'Rome'],
+    	answer: 1
+ 	},
+	{
+		question: 'What is the Capital of Utah?',
+    	options: ['Salt Lake City', 'Berlin', 'Madrid', 'Rome'],
+    	answer: 1
+ 	}]
+
+questionText.textContent = questions[0].question;
